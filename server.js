14988b469ffccd1bb69cc27f821a0fc978455138
Daniel Felipe Garcia Forero <yodanielgarcia@pop-os.localdomain>
2019-12-15 23:20:07 -0500

    heroku deploy

--- server.js
+++ server.js
@@ -1,16 +1,7 @@
-//Install express server
 const express = require('express');
+const serveStatic = require("serve-static")
 const path = require('path');
-
-const app = express();
-
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/sunmedia'));
-
-app.get('/*', function (req, res) {
-
-  res.sendFile(path.join(__dirname, '/dist/sunmedia/index.html'));
-});
-
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app = express();
+app.use(serveStatic(path.join(__dirname, 'dist')));
+const port = process.env.PORT || 80;
+app.listen(port);
