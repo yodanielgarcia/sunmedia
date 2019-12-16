28d308c6d9729c1fe16b6e5e43700bfb71b66b04
unknown <danielfelipegarciaforero@gmail.com>
2019-12-16 11:01:44 -0500

    heroku

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
-const express = require('express');
-const path = require('path');
-const serveStatic = require('serve-static');
+const express = require('express')
+const serveStatic = require('serve-static')
+const path = require('path')
 
-let app = express();
-app.use(serveStatic(__dirname + "/dist"));
+const app = express()
 
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
-  console.log('Listening on port ' + port)
-});
+//here we are configuring dist to serve app files
+app.use('/', serveStatic(path.join(__dirname, '/dist')))
+
+// this * route is to serve project on different page routes except root `/`
+app.get(/.*/, function (req, res) {
+	res.sendFile(path.join(__dirname, '/dist/index.html'))
+})
+
+const port = process.env.PORT || 8080
+app.listen(port)
+console.log(`app is listening on port: ${port}`)
